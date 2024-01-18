ae08fd1333a9ab014f42010cfe6d3571d1dfe36f
dean-nunes <144335719+dean-nunes@users.noreply.github.com>
2024-01-18 12:14:51 +0100

    stukje js

---
+++ main.js
@@ -0,0 +1,8 @@
+//initial references
+const letterContainer = document.querySelector("letter-container");
+const optionsContainer = document.querySelector("options-container");
+const userInputSection = document.querySelector("user-input-section");
+const newGameContainer = document.querySelector("new-game-container");
+const newGameButton = document.querySelector("new-game-button");
+const canvas = document.querySelector("canvas");
+const resultText = document.querySelector("result-text");
