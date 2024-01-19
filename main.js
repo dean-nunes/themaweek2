ec0cd7bef54dd40dfd296354c1b8499268ec2c25
dean-nunes <144335719+dean-nunes@users.noreply.github.com>
2024-01-19 09:46:31 +0100

    grote update javascript

--- main.js
+++ main.js
@@ -43,3 +43,46 @@ const displayOption = () => {
     }
     optionsContainer.appendChild(buttonCon);
 };
+
+//block all the Buttons
+const blocker = () => {
+    let optionsButtons = document.querySelectorAll(".options");
+    let letterButtons = document.querySelectorAll(".letters");
+    //disable all options
+    optionsButtons.forEach((button) => {
+        button.disabled = true;
+    });
+
+    //disable all letters 
+    letterButtons.forEach((button) => {
+        button.disabled.true;
+    });
+    newGameContainer.classList.remove("hide");
+};
+
+//World Generator
+const generateWorld = (optionsValue) => {
+    let optionsButtons = document.querySelectorAll(".options");
+    //if optionsValur matches the button innertext then highlight the button
+    optionsButtons.forEach((button) => {
+        if (button.innerText.toLowerCase() === optionsValue) {
+            button.classList.add("active");
+        }
+        button.disabled = true;
+    });
+
+    //initially hide letters, clear previous word
+    letterContainer.classList.remove("hide");
+    userInputSection.innerText = "";
+
+    let optionArray = options[optionsValue];
+    //choose random word
+    chosenWord = optionArray[Math.floor(Math.random() * optionArray.lenght)];
+    chosenWord = chosenWord.toUpperCase();
+
+    //replace every letter with span containing dash
+    let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+
+    //display each element as span
+    userInputSection.innerHTML = displayItem;
+};
