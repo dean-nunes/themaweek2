d8657011f222f4174a39671e6dfe8f692d4083c6
dean-nunes <144335719+dean-nunes@users.noreply.github.com>
2024-01-19 10:10:21 +0100

    dd

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-//Initial References
-const letterContainer = document.getElementById("letter-container");
-const optionsContainer = document.getElementById("options-container");
-const userInputSection = document.getElementById("user-input-section");
-const newGameContainer = document.getElementById("new-game-container");
-const newGameButton = document.getElementById("new-game-button");
-const canvas = document.getElementById("canvas");
-const resultText = document.getElementById("result-text");
+//initial references
+const letterContainer = document.querySelector("#letter-container");
+const optionsContainer = document.querySelector("#options-container");
+const userInputSection = document.querySelector("#user-input-section");
+const newGameContainer = document.querySelector("#new-game-container");
+const newGameButton = document.querySelector("#new-game-button");
+const canvas = document.querySelector("#canvas");
+const resultText = document.querySelector("#result-text");
 
-//Options values for buttons
+//options values for butttons
 let options = {
-  fruits: [
-    "Apple",
-    "Blueberry",
-    "Mandarin",
-    "Pineapple",
-    "Pomegranate",
-    "Watermelon",
-  ],
-  animals: ["Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra"],
-  countries: [
-    "India",
-    "Hungary",
-    "Kyrgyzstan",
-    "Switzerland",
-    "Zimbabwe",
-    "Dominica",
-  ],
+    landen: [
+        "Portugal",
+        "Nederland",
+        "Engeland",
+        "China",
+        "Rusland",
+        "Marokko",
+    ],
+    kleuren: ["zwart", "blauw", "geel", "oranje", "groen", "rood"],
+    brood: [
+        "pita",
+        "volkoren",
+        "witbrood",
+        "bruinbrood",
+        "meergranen",
+        "glutenvrij",
+    ],
 };
 
 //count
@@ -35,75 +35,75 @@ let count = 0;
 let chosenWord = "";
 
 //Display option buttons
-const displayOptions = () => {
-  optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
-  let buttonCon = document.createElement("div");
-  for (let value in options) {
-    buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
-  }
-  optionsContainer.appendChild(buttonCon);
-};
-
-//Block all the Buttons
-const blocker = () => {
-  let optionsButtons = document.querySelectorAll(".options");
-  let letterButtons = document.querySelectorAll(".letters");
-  //disable all options
-  optionsButtons.forEach((button) => {
-    button.disabled = true;
-  });
-
-  //disable all letters
-  letterButtons.forEach((button) => {
-    button.disabled.true;
-  });
-  newGameContainer.classList.remove("hide");
-};
-
-//Word Generator
-const generateWord = (optionValue) => {
-  let optionsButtons = document.querySelectorAll(".options");
-  //If optionValur matches the button innerText then highlight the button
-  optionsButtons.forEach((button) => {
-    if (button.innerText.toLowerCase() === optionValue) {
-      button.classList.add("active");
+const displayOption = () => {
+    optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
+    let buttonCon = document.createElement("div");
+    for (let value in options) {
+        buttonCon.innerHTML += `<button class="options" onclick="generateWorld('${value}')">${value}</button>`;
     }
-    button.disabled = true;
-  });
-
-  //initially hide letters, clear previous word
-  letterContainer.classList.remove("hide");
-  userInputSection.innerText = "";
-
-  let optionArray = options[optionValue];
-  //choose random word
-  chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
-  chosenWord = chosenWord.toUpperCase();
-
-  //replace every letter with span containing dash
-  let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
-
-  //Display each element as span
-  userInputSection.innerHTML = displayItem;
+    optionsContainer.appendChild(buttonCon);
 };
 
-//Initial Function (Called when page loads/user presses new game)
+//block all the Buttons
+const blocker = () => {
+    let optionsButtons = document.querySelectorAll(".options");
+    let letterButtons = document.querySelectorAll(".letters");
+    //disable all options
+    optionsButtons.forEach((button) => {
+        button.disabled = true;
+    });
+
+    //disable all letters 
+    letterButtons.forEach((button) => {
+        button.disabled.true;
+    });
+    newGameContainer.classList.remove("hide");
+};
+
+//World Generator
+const generateWorld = (optionsValue) => {
+    let optionsButtons = document.querySelectorAll(".options");
+    //if optionsValur matches the button innertext then highlight the button
+    optionsButtons.forEach((button) => {
+        if (button.innerText.toLowerCase() === optionsValue) {
+            button.classList.add("active");
+        }
+        button.disabled = true;
+    });
+
+    //initially hide letters, clear previous word
+    letterContainer.classList.remove("hide");
+    userInputSection.innerText = "";
+
+    let optionArray = options[optionsValue];
+    //choose random word
+    chosenWord = optionArray[Math.floor(Math.random() * optionArray.lenght)];
+    chosenWord = chosenWord.toUpperCase();
+
+    //replace every letter with span containing dash
+    let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+
+    //display each element as span
+    userInputSection.innerHTML = displayItem;
+};
+
+//initial function (called when page loads/user presses new game)
 const initializer = () => {
-  winCount = 0;
-  count = 0;
+    winCount = 0;
+    count = 0;
 
-  //Initially erase all content and hide letteres and new game button
-  userInputSection.innerHTML = "";
-  optionsContainer.innerHTML = "";
-  letterContainer.classList.add("hide");
-  newGameContainer.classList.add("hide");
-  letterContainer.innerHTML = "";
+    //initially erase all content and hide letteres and new game button
+    userInputSection.innerHTML = "";
+    optionsContainer.innerHTML = "";
+    letterContainer.classList.add("hide");
+    newGameContainer.classList.add("hide");
+    letterContainer.innerHTML = "";
 
-  //For creating letter buttons
-  for (let i = 65; i < 91; i++) {
+//for creating letter buttons
+for (let i = 65; i < 91; i++) {
     let button = document.createElement("button");
     button.classList.add("letters");
-    //Number to ASCII[A-Z]
+    //number to ascii[a-z]
     button.innerText = String.fromCharCode(i);
     //character button click
     button.addEventListener("click", () => {
